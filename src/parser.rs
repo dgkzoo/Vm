7f5a1d118f1ef4c4c80ec81e5504929209b9b881
dgkzoo <dgookz@gmail.com>
2019-05-12 18:44:34 +0900

    push対応

--- src/parser.rs
+++ src/parser.rs
@@ -5,21 +5,49 @@
 /// dgkzoo
 ///
 
+pub const CMD_PUSH: &str = "PUSH";
+pub const CMD_POP: &str = "POP";
+
+
 ///
 /// パーサ
 ///
-pub struct Parser {}
+pub struct Parser {
+    org_line: String,
+    cmd: String,
+    arg1: String,
+    arg2: String
+}
 
 impl Parser {
-    pub fn new() -> Parser {
-        Parser {}
+    pub fn new(line:String) -> Parser {
+        Parser {
+            org_line: line,
+            cmd: "".to_string(),
+            arg1: "".to_string(),
+            arg2: "".to_string()
+         }
+    }
+
+    pub fn exec(&mut self) {
+        let line = self.get_valid_line().to_string();
+        // self.cmd = self.get_command_type().to_string();
+
+        let mut vals = line.split_whitespace().peekable();
+        self.cmd = vals.next().unwrap().to_string();
+        if vals.peek() != None { 
+            self.arg1 = vals.next().unwrap().to_string();
+        }
+        if vals.peek() != None { 
+            self.arg2 = vals.next().unwrap().to_string();
+        }
     }
 
     ///
     /// 空白行、コメントの場合、ブランク文字列を返す
     ///
-    pub fn get_valid_line(&self, line: &mut String) -> String {
-        let mut line = line.trim().to_string();
+    pub fn get_valid_line(&self) -> String {
+        let mut line = self.org_line.trim().to_string();
 
         if line.is_empty() {
             return "".to_string();
@@ -35,5 +63,27 @@ impl Parser {
         }
 
         return line.trim().to_string();
+    }
+    
+    ///
+    /// コマンドタイプを返す
+    ///
+    pub fn get_command_type(&self) -> &str {
+        if self.org_line.starts_with("push") {
+            return CMD_PUSH;
+        }
+        if self.org_line.starts_with("pop") {
+            return CMD_POP;
+        }
+
+        return "X";
+    }
+
+    pub fn get_arg1(&self) -> &str{
+        return &self.arg1;
+    }
+
+    pub fn get_arg2(&self) -> &str {
+        return &self.arg2;
     }
 }
--- src/vmtransrator.rs
+++ src/vmtransrator.rs
@@ -8,6 +8,7 @@ use std::fs;
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::Path;
 
+use parser;
 use parser::Parser;
 
 pub struct VmTransrator {
@@ -41,17 +42,53 @@ impl VmTransrator {
         for line in reader.lines() {
 
             // コメント、空白行などを除去
-            let parser = Parser::new();
-            let line = parser.get_valid_line(&mut line.unwrap());
+            let parser = Parser::new(line.unwrap());
+            let line = parser.get_valid_line();
             if line.is_empty() {
                 continue;
             }
 
-            println!("line = {}", line);
-
             // ファイルに出力
-            print!("{}", line);
-            out_buf.write(line.as_bytes()).unwrap();
+            let out_asm = self.vm2asm(line);
+            print!("out_asm = {}", out_asm);
+            out_buf.write(out_asm.as_bytes()).unwrap();
         }
+    }
+
+    pub fn vm2asm(&mut self, line: String) -> String{
+        let mut out_asm = "".to_string();
+
+        let mut parser = Parser::new(line);
+        parser.exec();
+        let cmd_type = parser.get_command_type();
+        let arg1 = String::from(parser.get_arg1());
+        let arg2 = String::from(parser.get_arg2());
+
+        // push, pop
+        if cmd_type == parser::CMD_PUSH || cmd_type == parser::CMD_POP {
+            out_asm = self.vm2asm_push_pop(cmd_type.to_string(), arg1, arg2);
+        }
+
+        return out_asm;
+    }
+
+    ///
+    /// pushコマンドをasmへ変換
+    /// 
+    pub fn vm2asm_push_pop(&mut self, command:String, segment:String, index:String) -> String {
+        let mut out_asm = "".to_string();
+
+        // constant値をpush
+        if segment == "constant" {
+            out_asm = out_asm + format!("@{}    // ***push constant {} ***\n", index, index).as_str();
+            out_asm = out_asm + format!("D=A    // D=A(constant {})\n", index).as_str();
+            out_asm = out_asm + format!("@SP    // Areg=0x00\n").as_str();
+            out_asm = out_asm + format!("A=M    // A=M[SP]\n").as_str();
+            out_asm = out_asm + format!("M=D    // push ({}) // M[SP]=D(constant {})\n", index, index).as_str();
+            out_asm = out_asm + format!("@SP    // Areg=0x00\n").as_str();
+            out_asm = out_asm + format!("M=M+1  // SP inc // M[SP]=M[SP]+1\n").as_str();
+        }
+
+        return out_asm;
     }
 }
